f485495050eec5913204cb49124dcfee0d087eb4
Sani Mustapha <mustapha3341@gmail.com>
2020-12-10 12:48:45 +0000

    init question and header components

---
+++ src/components/Question/Question.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Question = (props) => {
+    return (
+        <div className="question">
+            <h1>Question</h1>
+        </div>
+    );
+};
+
+export default Question;
