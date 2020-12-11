c4be1ba46c8b59ac072d55dd7006b00d0943169c
Sani Mustapha <mustapha3341@gmail.com>
2020-12-11 15:47:31 +0000

    fixed re-render issue with shuffleAnswers

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -1,20 +1,62 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { shuffle } from "lodash";
 
 const Question = ({ questions }) => {
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [answers, setAnswers] = useState([]);
+    const [selectedAnswer, setSelectedAnswer] = useState("");
+    const [correctAnswer, setCorrectAnswer] = useState("");
+
+    const shuffledAnswers = useCallback(() => {
+        const { correct_answer, incorrect_answers } = questions[
+            currentQuestion
+        ];
+        const answers = shuffle([correct_answer, ...incorrect_answers]);
+        return answers;
+    }, [questions,currentQuestion]);
+    
+
+    useEffect(() => {
+        const ans = shuffledAnswers();
+        setAnswers(ans);
+    }, [shuffledAnswers])
+
+    const handleClick = (index) => {
+        shuffledAnswers();
+        setSelectedAnswer(answers[index]);
+
+        // check if answer is correct
+        if(selectedAnswer === questions[currentQuestion].correct_answer) {
+            setCorrectAnswer(selectedAnswer);
+            alert("WHoaahh!!!! 😁");
+        }
+
+        const nextQuestion = currentQuestion + 1;
+        if (nextQuestion < questions.length) {
+            setCurrentQuestion(nextQuestion);
+        } else {
+            return;
+        }
+    };
+
     return (
         <div className="question">
             <div className="question--box">
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Nam, possimus? Eum dolorum quo animi nostrum!
-                </p>
+                <p
+                    dangerouslySetInnerHTML={{
+                        __html: questions[currentQuestion].question,
+                    }}
+                ></p>
             </div>
             <div className="answers--container">
-                <button className="answer">Answer</button>
-                <button className="answer">Answer</button>
-                <button className="answer">Answer</button>
-                <button className="answer">Answer</button>
+                {answers.map((answer, index) => (
+                    <button
+                        onClick={() => handleClick(index)}
+                        key={answer}
+                        dangerouslySetInnerHTML={{ __html: answer }}
+                        className="answer"
+                    ></button>
+                ))}
             </div>
         </div>
     );
